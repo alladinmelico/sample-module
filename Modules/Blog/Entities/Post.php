9029b0c7c3c6a3aa686e69299995b8f97145b3ff
Alladin Melico <melico.alladin@gmail.com>
2022-06-14 10:39:08 +0800

    initial commit

---
+++ Modules/Blog/Entities/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Blog\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'content'
+    ];
+
+    protected static function newFactory()
+    {
+        return \Modules\Blog\Database\factories\PostFactory::new();
+    }
+}
